b463a1686077a16735706de2c80ee7c823d4f6c8
MilleFlowers <geral.mille.flowers@gmail.com>
2026-03-11 22:42:03 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -998,4 +998,5 @@ def gerir_cores(produto_id):
 init_db()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
