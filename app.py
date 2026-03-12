28ec8fa444081b67712d03c686fa19563aabefd8
MilleFlowers <geral.mille.flowers@gmail.com>
2026-03-12 08:47:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = os.environ.get("SECRET_KEY", "mille_secret_key")
 stripe.api_key = os.environ.get("STRIPE_API_KEY", "SUA_CHAVE_SECRETA_AQUI")
 
 # Email do administrador
-ADMIN_EMAIL = "filipenetocunha@gmail.com"
+ADMIN_EMAIL = "camillealmeida2019@gmail.com"
 
 # ---------------- Context processor ----------------
 
@@ -999,4 +999,5 @@ init_db()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+
     app.run(host="0.0.0.0", port=port, debug=True)
